0c7abd89a97f78d361879baad5e1793a71980649
Nolan Neustaeter <nolan@flexdealer.com>
2016-08-11 16:31:01 -0700

    Fixed an issue with non camelcased styles and removed the base option

--- src/lifter.js
+++ src/lifter.js
@@ -2,24 +2,24 @@ class Lifter {
 
 	constructor(options) {
 
+		let _L = this;
+
 		let settings = {
-			base : 'body',
 			triggers : '[target=_popup]',
 			preload : true
 		}
 
 		Object.assign(settings, options);
 
-		this.base = document.querySelector(settings.base);
-		var base = this.base;
+		_L.base = document.querySelector('body');
 
-		this.triggers = document.querySelectorAll(settings.triggers);
+		_L.triggers = document.querySelectorAll(settings.triggers);
 
-		this.base.classList.add('lifter-base');
+		_L.base.classList.add('lifter-base');
 
-		this.triggers.forEach(function(trigger) {
+		_L.triggers.forEach(function(trigger) {
 			trigger.classList.add('lifter-trigger');
-		}, this);
+		}, _L);
 
 		if (settings.preload !== true) {
 			let lifterCommon = document.createElement('aside');
@@ -27,17 +27,17 @@ class Lifter {
 			lifterCommon.id = 'lifter-common';
 			lifterCommon.classList.add('lifter');
 
-			this.base.appendChild(lifterCommon);
+			_L.base.appendChild(lifterCommon);
 		}
 
 		if (settings.preload !== false) {
 			if (settings.preload === true) {
-				this.preloads = this.triggers;
+				_L.preloads = _L.triggers;
 			} else {
-				this.preloads = document.querySelectorAll(settings.preload);
+				_L.preloads = document.querySelectorAll(settings.preload);
 			}
 
-			this.preloads.forEach(function(trigger) {
+			_L.preloads.forEach(function(trigger) {
 				if (trigger.getAttribute('data-content') === null) {
 					let lifter = document.createElement('aside'),
 						frame = document.createElement('iframe');
@@ -49,16 +49,17 @@ class Lifter {
 					frame.src = trigger.href;
 
 					lifter.appendChild(frame);
-					base.appendChild(lifter);
+					_L.base.appendChild(lifter);
 
 					trigger.setAttribute('data-lifter', '#' + lifter.id);
 				}
-			}, this);
+			});
 		}
 
-		this.base.addEventListener('click', function(event) {
+		_L.base.addEventListener('click', function(event) {
 			if (event.target.classList.contains('lifter-trigger')) {
 				event.preventDefault();
+
 				let trigger = event.target,
 					target = trigger.getAttribute('data-lifter');
 
@@ -66,64 +67,132 @@ class Lifter {
 					target = '#lifter-common';
 				}
 
-				let lifter = document.querySelector(target);
+				let config = {
+					target: target,
+					classes: [],
+					styles: {}
+				};
+
 
 				// size of popup
 				if (trigger.classList.contains('lifter-sm')) {
-					lifter.classList.add('lifter-sm');
+					config.classes.push('lifter-sm');
 				} else if (trigger.classList.contains('lifter-lg')) {
-					lifter.classList.add('lifter-lg');
+					config.classes.push('lifter-lg');
 				}
 
 				if (trigger.getAttribute('data-width') !== null) {
-					lifter.style.width = trigger.getAttribute('data-width');
+					config.styles.width = trigger.getAttribute('data-width');
 				}
 				if (trigger.getAttribute('data-height') !== null) {
-					lifter.style.height = trigger.getAttribute('data-height');
+					config.styles.height = trigger.getAttribute('data-height');
 				}
 
 				if (trigger.getAttribute('data-size')) {
 					let size = trigger.getAttribute('data-size').split('|');
-					lifter.style.width = size[0];
-					lifter.style.height = size[1];
+					config.styles.width = size[0];
+					config.styles.height = size[1];
 				}
 
 				// content of popup
 				if (trigger.getAttribute('data-content') !== null) {
-					lifter.innerHtml = document.querySelector(trigger.getAttribute('data-content')).innerHtml;
+					config.html = document.querySelector(trigger.getAttribute('data-content')).innerHtml;
 				} else if (trigger.getAttribute('data-lifter') === null) {
-					let frame = document.createElement('iframe');
 
-					frame.classList.add('lifter-content');
-					frame.src = trigger.href;
 
-					lifter.appendChild(frame);
+					config.src = trigger.href;
 				}
 
-				base.classList.add('lifting');
-				lifter.classList.add('lifted');
+				_L.lift(config);
 			}
 		});
 
 
-		this.base.addEventListener('click', function(event) {
+		_L.base.addEventListener('click', function(event) {
 			if (event.target.classList.contains('lifting')) {
-				let lifter = base.querySelector('.lifted');
-
-				if (lifter.id === 'lifter-common') {
-					lifter.innerHtml = '';
-				}
-
-				lifter.classList.remove('lifted');
-				base.classList.remove('lifting');
-
-				setTimeout(function() {
-					lifter.classList.remove('lifter-sm');
-					lifter.classList.remove('lifter-lg');
-					lifter.style.width = '';
-					lifter.style.height = '';
-				}, 350);
+				_L.lower();
 			}
 		});
+	}
+
+	lift(options) {
+		var _L = this;
+
+		let settings = {
+			target: '#lifter-common',
+			html: null,
+			src: null,
+			classes: [],
+			styles: {}
+		}
+
+		Object.assign(settings, options);
+
+		let lifter = document.querySelector(settings.target);
+
+		// classes
+		settings.classes.forEach(function(lifterClass) {
+			lifter.classList.add(lifterClass);
+		});
+		lifter.setAttribute('data-added-classes', settings.classes.join(' '));
+
+		// styles
+		Object.keys(settings.styles).forEach(function(key) {
+			let words = key.split('-'),
+				style = '';
+
+			if (words.length > 1) {
+				style = words[0].toLowerCase() + words.slice(1).map(function(word) {
+					return (
+						word.charAt(0).toUpperCase() + word.slice(1)
+					);
+				}).join();
+			} else {
+				style = key;
+			}
+
+			lifter.style[style] = settings.styles[key];
+		});
+
+		if (settings.html !== null) {
+			lifter.innerHtml = settings.html;
+		} else if (settings.src !== null) {
+			let frame = document.createElement('iframe');
+
+			frame.classList.add('lifter-content');
+			frame.src = trigger.href;
+
+			lifter.appendChild(frame);
+		} else if (settings.target === '#lifter-common') {
+			return false; // no content provided to lift
+		}
+
+		_L.base.classList.add('lifting');
+		lifter.classList.add('lifted');
+
+		return lifter;
+	}
+
+	lower() {
+		var _L = this;
+
+		let lifter = _L.base.querySelector('.lifted');
+
+		if (lifter.id === 'lifter-common') {
+			lifter.innerHtml = '';
+		}
+
+		lifter.classList.remove('lifted');
+		_L.base.classList.remove('lifting');
+
+		setTimeout(function() {
+			if (lifter.getAttribute('data-added-classes').trim() !== '') {
+				lifter.getAttribute('data-added-classes').split(' ').forEach(function(lifterClass) {
+					lifter.classList.remove(lifterClass);
+				});
+			}
+			lifter.setAttribute('style', '')
+
+		}, 350);
 	}
 }
